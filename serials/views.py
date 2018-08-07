c88921fa83185e5199bb09c7de12d3b545d11ed2
navicester <navicester@qq.com>
2018-08-07 20:31:15 +0800

    filter active course

--- serials/views.py
+++ serials/views.py
@@ -6,7 +6,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.core.urlresolvers import reverse
 class SerialListView(ListView):
-    queryset = Serial.objects.all()
+    queryset = Serial.objects.filter(active=True)
     template_name = "serials_list.html"
 
 class SerialDetailView(DetailView):
@@ -15,6 +15,6 @@ class SerialDetailView(DetailView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(SerialDetailView, self).get_context_data(*args, **kwargs) 
-        context["object_list"] = Entry.objects.filter(serial__pk=self.get_object().pk)
+        context["object_list"] = Entry.objects.filter(serial__pk=self.get_object().pk, active=True)
 
         return context
