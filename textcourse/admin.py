efef743d23936ecd2fca920e0dd0c64683b2a33c
navicester <navicester@qq.com>
2018-08-12 15:09:49 +0800

    update admin

--- textcourse/admin.py
+++ textcourse/admin.py
@@ -3,5 +3,28 @@ from mptt.admin import MPTTModelAdmin
 from .models import MPTTArticle, Course
 # Register your models here.
  
-admin.site.register(MPTTArticle, MPTTModelAdmin)
-admin.site.register(Course)
+class MPTTArticleInline(admin.TabularInline):
+
+    model = MPTTArticle
+    extra = 1
+    max_num = 50
+
+class MPTTArticleAdmin(MPTTModelAdmin):
+    list_display = [
+        'course',
+        'order',
+        'title',
+    ]
+
+    list_filter = [
+        'course'
+    ]
+
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [
+        # MPTTArticleInline,
+    ]
+
+admin.site.register(MPTTArticle, MPTTArticleAdmin)
+admin.site.register(Course, CourseAdmin)
