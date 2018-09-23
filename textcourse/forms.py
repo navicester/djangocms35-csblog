2492d5274c60d382868bf754e1e169181ae7ebd8
navicester <navicester@qq.com>
2018-09-23 22:54:21 +0800

    fix bug

--- textcourse/forms.py
+++ textcourse/forms.py
@@ -7,30 +7,38 @@ class ArticleForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ArticleForm, self).__init__(*args, **kwargs)
 
-        # course = None
-        # instance = getattr(self, 'instance', None)
-        # # print instance
+        # parent_init = self.initial['parent']
+        # print type(parent_init)
+        # parent_choice = self.fields['parent'].choices
+        # for _ in parent_choice:
+        #     print _
 
-        # if instance and hasattr(instance, 'course'):
-        #     course = instance.course
+        course = None
+        instance = getattr(self, 'instance', None)
 
-        # if course:
-        #     try:
-        #         CHOICE_LIST = []
-        #         for ins in MPTTArticle.objects.filter(course=course): #course.article_set.all():
-        #             if not (ins, ins) in CHOICE_LIST:
-        #                 CHOICE_LIST.append((ins, ins))
-        #         # CHOICE_LIST.sort()
-        #         CHOICE_LIST.insert(0, ('', '----'))
-  
-        #         self.fields['parent'].choices = CHOICE_LIST
+        if instance and hasattr(instance, 'course'):
+            course = instance.course
 
-        #         # OR
-        #         # self.fields['surface'].choices.insert(0,('', _('Please choose a surface')))
+        if course:
+            try:
+                CHOICE_LIST = []
+                for ins in MPTTArticle.objects.filter(course=course): #course.article_set.all():
+                    if not (ins, ins) in CHOICE_LIST:
+                        CHOICE_LIST.append((ins.id, ins))
+                # CHOICE_LIST.sort()
+                CHOICE_LIST.insert(0, ('', '----'))  
+                self.fields['parent'].choices = CHOICE_LIST
 
-        #         # https://stackoverflow.com/questions/16464119/django-insert-choice-is-neglected-in-form-init                
-        #     except:
-        #         pass
+                # obj = MPTTArticle.objects.filter(id=parent_id).first()
+                # self.initial['parent'] = obj
+                # self.initial['parent'] = parent_init
+
+                # OR
+                # self.fields['surface'].choices.insert(0,('', _('Please choose a surface')))
+
+                # https://stackoverflow.com/questions/16464119/django-insert-choice-is-neglected-in-form-init                
+            except:
+                pass
 
     class Meta: 
         model = MPTTArticle 
