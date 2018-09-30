75bf0c6741a760f7153558b8d07d8a52544ef440
navicester <navicester@qq.com>
2018-09-30 23:21:04 +0800

    set active attribute for course and article

--- textcourse/models.py
+++ textcourse/models.py
@@ -10,6 +10,18 @@ from django.utils.safestring import mark_safe
 
 # Create your models here.
 
+class CourseQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+class CourseManager(models.Manager):
+    def get_queryset(self):
+        return CourseQuerySet(self.model, using=self._db)
+
+    def all(self, *args, **kwargs):
+        return self.get_queryset().active()
+
+
 class Course(models.Model):
     title = models.CharField(_('title'), max_length=150, blank=False, null=False)
     abstract = models.TextField(_('abstract'), max_length=500, blank=True, null=True)
@@ -19,6 +31,7 @@ class Course(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
+    objects = CourseManager()
 
     def __unicode__(self):
         return self.title
@@ -37,6 +50,7 @@ class Article(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
     order = models.IntegerField(_("order"), blank=True, null=True, default=-1)
+    active = models.BooleanField(_("active"), default=True)
     updated = models.DateTimeField(auto_now=False, auto_now_add=True)
     timestap = models.DateTimeField(auto_now=True, auto_now_add=False)
 
@@ -67,8 +81,14 @@ class Article(models.Model):
 
 
 class ArticleManager(models.Manager):
+    def get_queryset(self):
+        return models.query.QuerySet(self.model, using=self._db)
+
     def root(self, *args, **kwargs):
-        return super(ArticleManager,self).filter(parent=None)
+        return super(ArticleManager,self).filter(parent=None).filter(active=True)
+
+    def all(self, *args, **kwargs):
+        return self.get_queryset().filter(active=True)
 
 class MPTTArticle(MPTTModel, Article):
 
